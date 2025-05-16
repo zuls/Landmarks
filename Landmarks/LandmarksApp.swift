3146ece85713a74d9ac0a926e544a17e1aa05d48
kamalksu <zkamal@students.kennesaw.edu>
2025-05-16 16:59:07 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Zularbine Kamal on 5/16/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
