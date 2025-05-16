fd3bc9472d81b5b730afdbdefbef49898aa0e130
zuls <zularbine@gmail.com>
2025-05-16 16:59:07 -0400

    Initial Commit

---
+++ Landmarks/ModelData.swift
@@ -0,0 +1,8 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by Zularbine Kamal on 5/17/25.
+//
+
+import Foundation
---
+++ Landmarks/Views/MapView.swift
@@ -0,0 +1,26 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Zularbine Kamal on 5/16/25.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map(initialPosition: .region(region))
+    }
+    
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 33.952301, longitude: -84.549049),
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    }
+}
+
+#Preview {
+    MapView()
+}
