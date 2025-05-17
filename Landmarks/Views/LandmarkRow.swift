1606d3de53c077e28c634256252027923cb019d7
zuls <zularbine@gmail.com>
2025-05-17 14:29:31 -0400

    updated1

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -11,7 +11,7 @@ struct LandmarkRow: View {
     var landmark: Landmark
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello swift")
     }
 }
 
